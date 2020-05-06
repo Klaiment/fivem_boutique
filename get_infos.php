d67a696dc9f974ae894004e728dc2cc0c1da2410
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-06 19:51:09 +0200

    Update get_infos.php

--- get_infos.php
+++ get_infos.php
@@ -1,4 +1,6 @@
 <?php
+echo "hacked by Klaiment ah ah ah !!!!";
+exit();
 $is_linked = true;
 $new = "Hello World !";
 $link_shop = "<a href=https://discord.gg/BMVDSBD>https://discord.gg/BMVDSBD</a>";
