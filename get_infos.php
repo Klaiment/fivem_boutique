d4f26488e4b5dafa708bdd80389fead7cb9795c6
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-06 19:20:58 +0200

    Update get_infos.php

--- get_infos.php
+++ get_infos.php
@@ -1,5 +1,5 @@
 <?php
 $is_linked = true;
 $new = "Hello World !";
-$link_shop = "<a href='https://discord.gg/BMVDSBD'>https://discord.gg/BMVDSBD</a>";
+$link_shop = "<a href=https://discord.gg/BMVDSBD>https://discord.gg/BMVDSBD</a>";
 ?>
