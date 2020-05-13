4476c526788ee32167253470fdd148bece87420f
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-13 19:09:21 +0200

    Update get_infos.php

--- get_infos.php
+++ get_infos.php
@@ -3,5 +3,9 @@ require 'config.php';
 $is_linked = true;
 $new = "Hello World !";
 $link_shop = "<a href=https://discord.gg/BMVDSBD>https://discord.gg/BMVDSBD</a>";
-
+if ($license_key == "123") {
+    echo "License OK";
+}else{
+exit();
+}
 ?>
