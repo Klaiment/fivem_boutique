fb41d1b388e6a9dd2c34eb05cba2bfe89116f944
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-13 19:23:25 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,9 +1,7 @@
 <?php
-$arr = array(
-    '123' => '446556',
-    '798' => '321'
-);
-
-
+        $remote = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+        $remote->exec("set names utf8");
+        $remote->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
+        $remote->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 ?>
