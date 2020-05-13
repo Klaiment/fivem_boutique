dcb3c70b0614a9ee07f5eef77d207e622d33d16d
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-13 19:28:57 +0200

    Update check.php

--- check.php
+++ check.php
@@ -1,5 +1,5 @@
 <?php
-        $remote = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+        $remote = new PDO("mysql:host=sql307.epizy.com;dbname=epiz_25767514_klaiment", epiz_25767514, m6ja382ApeGFC9W);
         $remote->exec("set names utf8");
         $remote->setAttribute(PDO::ATTR_CASE,PDO::CASE_LOWER);
         $remote->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
