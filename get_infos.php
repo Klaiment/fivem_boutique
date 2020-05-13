9608857369670b07b3c93b08ad6b555096c75dc3
Clement <46396830+Klaiment@users.noreply.github.com>
2020-05-13 19:15:37 +0200

    Update get_infos.php

--- get_infos.php
+++ get_infos.php
@@ -2,9 +2,9 @@
 $is_linked = true;
 $new = "Hello World !";
 $link_shop = "<a href=https://discord.gg/BMVDSBD>https://discord.gg/BMVDSBD</a>";
-/*if ($license_key == "123") {
+if ($license_key == "123") {
     echo "License OK";
 }else{
 exit();
-}*/
+}
 ?>
